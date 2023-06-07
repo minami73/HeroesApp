3fe8f3b0f6acceca4b177b0aeeecbbf9e4ed7fc7
minami73 <jj.c.r.1781@gmail.com>
2023-06-07 14:49:11 -0600

    Sección 16 terminada

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,17 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/auth/interfaces/user.interface';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
-  selector: 'app-layout-page',
-  templateUrl: './layout-page.component.html',
-  styles: [
-  ]
+    selector: 'app-layout-page',
+    templateUrl: './layout-page.component.html',
+    styles: [
+    ]
 })
 export class LayoutPageComponent {
 
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ) { }
+
     public sidebarItems = [
-        { label: 'Listado', icon: 'label', url: './list'},
-        { label: 'Añadir', icon: 'add', url: './new-hero'},
-        { label: 'Buscar', icon: 'search', url: './search'}
+        { label: 'Listado', icon: 'label', url: './list' },
+        { label: 'Añadir', icon: 'add', url: './new-hero' },
+        { label: 'Buscar', icon: 'search', url: './search' }
     ]
+
+    get user(): User | undefined {
+        return this.authService.currentuser;
+    }
+
+    onLogout(): void {
+        this.authService.logout();
+        this.router.navigate(['/auth/login'])
+    }
 
 }
